a83933eee9b64e6fbc1aaff55aef14b63664219f
fernandoangelesjr <fernandoangelesjr_02@live.com>
2024-05-02 01:32:33 +0800

    added authorization (global)

--- lib/api/restapiadapter.dart
+++ lib/api/restapiadapter.dart
@@ -1,16 +1,24 @@
 import 'dart:developer';
 import 'package:http/http.dart' as http;
 import 'package:party_games_by_tdng/api/appsettings.dart';
+import 'package:party_games_by_tdng/helpers/jwttokenhelper.dart';
 
 class RestApiAdapter {
   static Future<String?> executeEndpoint(
       String endpoint, String request) async {
+    String token = generateJwtToken();
+
     try {
       var url = Uri.parse(baseUrl + endpoint);
-      var response = await http.post(url,
-          body: request, headers: {"Content-Type": "application/json"});
+      var response = await http.post(url, body: request, headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer $token"
+      });
+
       if (response.statusCode == 200) {
         return response.body;
+      } else if (response.statusCode == 401) {
+        //print("unauthorized");
       }
     } catch (e) {
       log(e.toString());
