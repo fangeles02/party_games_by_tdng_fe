0f9ac006e9dc1bb174b77c7a03f745aa348bdfd4
fernandoangelesjr <fernandoangelesjr_02@live.com>
2024-05-02 01:10:36 +0800

    update appsettings

--- lib/api/appsettings.dart
+++ lib/api/appsettings.dart
@@ -1,12 +1,7 @@
 import 'package:party_games_by_tdng/helpers/apienvironmenthelper.dart';
 
-ApiEnvironmentsEnum appEnvironment = ApiEnvironmentsEnum.testserver;
+ApiEnvironmentsEnum appEnvironment = ApiEnvironmentsEnum.testlocalhost;
 String baseUrl = GetBaseUrl(appEnvironment);
-
-class ApiConstants {
-  //api endpoints
-  static String sampleEndpoint = '/WeatherForecast/Sample';
-}
 
 class SignalrConstants {
   //hubs
